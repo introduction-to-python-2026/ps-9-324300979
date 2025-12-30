3133efd020faa638cce1162bb3ad2415d3102e56
324300979 <yaelarbel2002@gmail.com>
2025-12-30 22:47:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,14 @@
-
 import lab_setup_do_not_edit
 
 import pandas as pd
 
 df = pd.read_csv('parkinsons.csv')
 df = df.dropna()
-df.head()
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 sns.pairplot(df, hue='status')
-
-plt.show()
 
 selected_features = ['NHR', 'PPE']
 
@@ -22,24 +18,17 @@ y = df['status']
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 X_scaled = scaler.fit_transform(X)
-print(X_scaled[:5])
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=42)
-print(f'x_train shape: {x_train.shape}')
-print(f'x_test shape: {x_test.shape}')
-print(f'y_train shape: {y_train.shape}')
-print(f'y_test shape: {y_test.shape}')
 
 from sklearn.svm import SVC
 model = SVC()
 model.fit(X_train, y_train)
-model
 
 from sklearn.metrics import accuracy_score
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
-accuracy
 
 import joblib
 
